f2903e6de63390e14d36a5aca2b2f16f6c9643a8
Nada8773 <nada2478@gmail.com>
2020-07-30 16:16:21 +0200

    Add Week7 introduction to self driving cars

---
+++ Introduction to Self-Driving Cars/Week7/Course1FinalProject/controller2d.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+
+"""
+2D Controller Class to be used for the CARLA waypoint follower demo.
+"""
+
+import cutils
+import numpy as np
+
+class Controller2D(object):
+    def __init__(self, waypoints):
+        self.vars                = cutils.CUtils()
+        self._current_x          = 0
+        self._current_y          = 0
+        self._current_yaw        = 0
+        self._current_speed      = 0
+        self._desired_speed      = 0
+        self._current_frame      = 0
+        self._current_timestamp  = 0
+        self._start_control_loop = False
+        self._set_throttle       = 0
+        self._set_brake          = 0
+        self._set_steer          = 0
+        self._waypoints          = waypoints
+        self._conv_rad_to_steer  = 180.0 / 70.0 / np.pi
+        self._pi                 = np.pi
+        self._2pi                = 2.0 * np.pi
+
+    def update_values(self, x, y, yaw, speed, timestamp, frame):
+        self._current_x         = x
+        self._current_y         = y
+        self._current_yaw       = yaw
+        self._current_speed     = speed
+        self._current_timestamp = timestamp
+        self._current_frame     = frame
+        if self._current_frame:
+            self._start_control_loop = True
+
+    def update_desired_speed(self):
+        min_idx       = 0
+        min_dist      = float("inf")
+        desired_speed = 0
+        for i in range(len(self._waypoints)):
+            dist = np.linalg.norm(np.array([
+                    self._waypoints[i][0] - self._current_x,
+                    self._waypoints[i][1] - self._current_y]))
+            if dist < min_dist:
+                min_dist = dist
+                min_idx = i
+        if min_idx < len(self._waypoints)-1:
+            desired_speed = self._waypoints[min_idx][2]
+        else:
+            desired_speed = self._waypoints[-1][2]
+        self._desired_speed = desired_speed
+
+    def update_waypoints(self, new_waypoints):
+        self._waypoints = new_waypoints
+
+    def get_commands(self):
+        return self._set_throttle, self._set_steer, self._set_brake
+
+    def set_throttle(self, input_throttle):
+        # Clamp the throttle command to valid bounds
+        throttle           = np.fmax(np.fmin(input_throttle, 1.0), 0.0)
+        self._set_throttle = throttle
+
+    def set_steer(self, input_steer_in_rad):
+        # Covnert radians to [-1, 1]
+        input_steer = self._conv_rad_to_steer * input_steer_in_rad
+
+        # Clamp the steering command to valid bounds
+        steer           = np.fmax(np.fmin(input_steer, 1.0), -1.0)
+        self._set_steer = steer
+
+    def set_brake(self, input_brake):
+        # Clamp the steering command to valid bounds
+        brake           = np.fmax(np.fmin(input_brake, 1.0), 0.0)
+        self._set_brake = brake
+
+    def update_controls(self):
+        ######################################################
+        # RETRIEVE SIMULATOR FEEDBACK
+        ######################################################
+        x               = self._current_x
+        y               = self._current_y
+        yaw             = self._current_yaw
+        v               = self._current_speed
+        self.update_desired_speed()
+        v_desired       = self._desired_speed
+        t               = self._current_timestamp
+        waypoints       = self._waypoints
+        throttle_output = 0
+        steer_output    = 0
+        brake_output    = 0
+
+        ######################################################
+        ######################################################
+        # MODULE 7: DECLARE USAGE VARIABLES HERE
+        ######################################################
+        ######################################################
+        """
+            Use 'self.vars.create_var(<variable name>, <default value>)'
+            to create a persistent variable (not destroyed at each iteration).
+            This means that the value can be stored for use in the next
+            iteration of the control loop.
+
+            Example: Creation of 'v_previous', default value to be 0
+            self.vars.create_var('v_previous', 0.0)
+
+            Example: Setting 'v_previous' to be 1.0
+            self.vars.v_previous = 1.0
+
+            Example: Accessing the value from 'v_previous' to be used
+            throttle_output = 0.5 * self.vars.v_previous
+        """
+        self.vars.create_var('v_previous', 0.0)
+
+        self.vars.create_var('error_previous', 0.0)
+        self.vars.create_var('t_previous', 0.0)
+        self.vars.create_var('v_Integral_error_previous', 0.0)
+        self.vars.create_var('v_derivative_error_previous', 0.0)
+
+        # Skip the first frame to store previous values properly
+        if self._start_control_loop:
+
+
+
+            """
+                Controller iteration code block.
+
+                Controller Feedback Variables:
+                    x               : Current X position (meters)
+                    y               : Current Y position (meters)
+                    yaw             : Current yaw pose (radians)
+                    v               : Current forward speed (meters per second)
+                    t               : Current time (seconds)
+                    v_desired       : Current desired speed (meters per second)
+                                      (Computed as the speed to track at the
+                                      closest waypoint to the vehicle.)
+                    waypoints       : Current waypoints to track
+                                      (Includes speed to track at each x,y
+                                      location.)
+                                      Format: [[x0, y0, v0],
+                                               [x1, y1, v1],
+                                               ...
+                                               [xn, yn, vn]]
+                                      Example:
+                                          waypoints[2][1]: 
+                                          Returns the 3rd waypoint's y position
+
+                                          waypoints[5]:
+                                          Returns [x5, y5, v5] (6th waypoint)
+                
+                Controller Output Variables:
+                    throttle_output : Throttle output (0 to 1)
+                    steer_output    : Steer output (-1.22 rad to 1.22 rad)
+                    brake_output    : Brake output (0 to 1)
+            """
+
+            ######################################################
+            ######################################################
+            # MODULE 7: IMPLEMENTATION OF LONGITUDINAL CONTROLLER HERE
+            ######################################################
+            ######################################################
+            """
+                Implement a longitudinal controller here. Remember that you can
+                access the persistent variables declared above here. For
+                example, can treat self.vars.v_previous like a "global variable".
+            """
+            
+            # Change these outputs with the longitudinal controller. Note that
+            # brake_output is optional and is not required to pass the
+            # assignment, as the car will naturally slow down over time.
+            throttle_output = 0
+            brake_output    = 0
+
+            Kp = 1.0
+            Ki = 0.2
+            Kd = 0.01
+
+            dt = t - self.vars.t_previous
+            '''
+              Assume that throttle is acceleration
+              
+              D_error = (d/dt) e(t))  =  -e(t) + r(t) 
+              
+            '''
+            v_error             = v_desired - v
+            v_Integral_error    = (v_error * dt + self.vars.v_Integral_error_previous)
+            v_derivative_error  = (v_error - self.vars.v_derivative_error_previous) /dt
+
+
+            throttle_output = (Kp * v_error) + (Ki * v_Integral_error) + (Kd * v_derivative_error)
+
+            ######################################################
+            ######################################################
+            # MODULE 7: IMPLEMENTATION OF LATERAL CONTROLLER HERE
+            ######################################################
+            ######################################################
+            """
+                Implement a lateral controller here. Remember that you can
+                access the persistent variables declared above here. For
+                example, can treat self.vars.v_previous like a "global variable".
+            """
+            
+            # Change the steer output with the lateral controller. 
+            steer_output    = 0
+
+            '''
+             By Using Pure Pursuit Algorithm 
+            '''
+            Ld_min = 10
+            index_Ldmin = 0
+            Kdd =0.8
+            L = 3   #Vehicle Length
+
+            y_rear = y - np.sin(yaw) * (L/2)
+            x_rear =  x - np.cos(yaw) * (L/2)
+
+            Ld_min = max(Kdd * v, Ld_min)
+
+            for i in range(len(waypoints)):
+
+                Ld_Square = ( (waypoints[i][1] - y_rear)**2 + ((waypoints[i][0] - x_rear)**2) )
+                Ld = Ld_Square /2
+                if Ld < Ld_min :
+                    continue
+                else:
+                    Ld = Ld_min
+                    index_Ldmin = i
+                    break
+            """
+            find the distance from my current position (x, y).
+            If the distance is less than my "ld", then go to the next waypoint
+            """
+            alpha_hat = np.arctan2( (waypoints[index_Ldmin][1] - y_rear) , (waypoints[index_Ldmin][0] - x_rear) )
+            alpha = alpha_hat - yaw
+
+
+            steer_output = np.arctan2( 2 * L * np.sin(alpha) , Ld )
+
+            ######################################################
+            # SET CONTROLS OUTPUT
+            ######################################################
+            self.set_throttle(throttle_output)  # in percent (0 to 1)
+            self.set_steer(steer_output)        # in rad (-1.22 to 1.22)
+            self.set_brake(brake_output)        # in percent (0 to 1)
+
+        ######################################################
+        ######################################################
+        # MODULE 7: STORE OLD VALUES HERE (ADD MORE IF NECESSARY)
+        ######################################################
+        ######################################################
+        """
+            Use this block to store old values (for example, we can store the
+            current x, y, and yaw values here using persistent variables for use
+            in the next iteration)
+        """
+        self.vars.v_previous = v  # Store forward speed to be used in next step
+
+        self.vars.error_previous = v_error
+        self.vars.t_previous     = t
+        self.vars.v_Integral_error_previous   = v_Integral_error
+        self.vars.v_derivative_error_previous = v_error
+
---
+++ Introduction to Self-Driving Cars/Week7/README.md
@@ -0,0 +1,43 @@
+## Implementation
+
+For this project i used 
+
+#### longitudinal vehicle Modle
+
+#### Lateral Vehicle Modle
+
+'''
+For this project i used  Pure pursuit is the geometric path tracking controller.
+A geometric path tracking controller is any controller that 
+tracks a reference path using only the geometry of the vehicle kinematics 
+and the reference path
+'''
+Image lateral3
+
+###### Formaluation
+
+Image lateral1
+Image lateral2
+
+
+## How to use
+- Windows 10 
+1. Run the carla simulator
+  - Use that command
+   - CarlaUE4.exe /Game/Maps/RaceTrack -windowed -carla-server -benchmark -fps=30
+2. Run Your Python Script 
+   - python module_7.py
+
+## Reference 
+
+'''
+- Empirical PID gain tuning (Kevin Lynch)
+https://www.youtube.com/watch?v=uXnDwojRb1g
+
+- PID 
+https://nicisdigital.wordpress.com/2011/06/27/proportional-integral-derivative-pid-controller/
+
+- Three Methods of Vehicle Lateral Control: Pure Pursuit, Stanley and MPC
+https://medium.com/@dingyan7361/three-methods-of-vehicle-lateral-control-pure-pursuit-stanley-and-mpc-db8cc1d32081
+
+'''
